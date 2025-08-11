3849c9094fd520470718cc55873ff9d30b0d91ae
1jsjs <pjs020201@naver.com>
2025-08-11 21:22:41 +0900

    [Bronze II] Title: 블랙잭, Time: 188 ms, Memory: 17592 KB -BaekjoonHub

---
+++ 백준/Bronze/2798. 블랙잭/블랙잭.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int N = scanner.nextInt();
+        int M = scanner.nextInt();
+
+        int [] arr = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = scanner.nextInt();
+        }
+
+        int result = search(arr,N,M);
+        System.out.println(result);
+    }
+
+    static int search(int [] arr, int N, int M) {
+        int result = 0;
+
+        for (int i = 0; i < N - 2; i++) {
+
+            if (arr[i] > M) continue;
+
+            for (int j = i + 1; j < N - 1; j++) {
+                
+                if (arr[i] + arr[j] > M) continue;
+
+                for (int k = j + 1; k < N; k++) {
+                    int temp = arr[i] + arr[j] + arr[k];
+
+                    if (M == temp) {
+                        return temp;
+                    }
+                    if (result < temp && temp < M) {
+                        result = temp;
+                    }
+                }
+            }
+        }
+
+        return result;
+    }
+}
